38414877e5b66d6f86c05e90c0d4b7571e64126d
Rokas <rookas.rudzenskas@gmail.com>
2021-04-07 09:09:47 +0300

    Rokas

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useState} from "react";
 import MainPageLayout from "../components/MainPageLayout";
 
 const Home = () => {
+    const [input, setInput] = useState('');
+
+    const onSearch = () => {
+        // https://api.tvmaze.com/search/shows?q=men
+        fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(response => response.json()).then(result => {
+            console.log(result);
+        });
+    };
+
+    const onInputChange = (ev) => {
+        setInput(ev.target.value);
+        // console.log(ev.target.value);
+    };
+
+    const onKeyDown = (ev) => {
+        if(ev.keyCode === 13){
+            onSearch();
+        }
+        // console.log(ev.keyCode);
+    };
+
     return (
-        <MainPageLayout>this is home</MainPageLayout>
-    )
+        <MainPageLayout>
+            <input type="text" onChange={onInputChange}  onKeyDown={onKeyDown} value={input} />
+            <button type="button" onClick={onSearch}>Search</button>
+        </MainPageLayout>
+    );
 };
 
 export default Home;
