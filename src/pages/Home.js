4d44f573df8290fd268f6025e633c2b3f096ac06
Rokas <rookas.rudzenskas@gmail.com>
2021-04-07 09:27:15 +0300

    Rokas

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,18 @@
 import React, { useState} from "react";
 import MainPageLayout from "../components/MainPageLayout";
+import {apiGet} from "../misc/config";
 
 const Home = () => {
     const [input, setInput] = useState('');
+    const [results, setResults] = useState(null);
 
     const onSearch = () => {
-        // https://api.tvmaze.com/search/shows?q=men
-        fetch(`https://api.tvmaze.com/search/shows?q=${input}`).then(response => response.json()).then(result => {
+
+        apiGet(`/search/shows?q=${input}`).then(result => {
+            setResults(result);
             console.log(result);
-        });
-    };
+            });
+        };
 
     const onInputChange = (ev) => {
         setInput(ev.target.value);
@@ -23,10 +26,28 @@ const Home = () => {
         // console.log(ev.keyCode);
     };
 
+    const renderResults = () => {
+        if(results && results.length === 0){
+            return <div>No results</div>
+        }
+
+        if(results && results.length > 0) {
+            return (<div>
+                    {results.map((item) => (
+                        <div key={item.show.id}>{item.show.name}</div>
+                    ))}
+                </div>
+            );
+        }
+
+        return null;
+    };
+
     return (
         <MainPageLayout>
             <input type="text" onChange={onInputChange}  onKeyDown={onKeyDown} value={input} />
             <button type="button" onClick={onSearch}>Search</button>
+            {renderResults()}
         </MainPageLayout>
     );
 };
