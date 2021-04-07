cbdca0e457861aec3f959018f2f099903b6b2540
Rokas <rookas.rudzenskas@gmail.com>
2021-04-07 08:08:25 +0300

    Rokas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { Switch, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div>Hi</div>
+      <Switch>
+    <Route exact path="/">This is home page</Route>
+
+    <Route exact path="/starred">This is starred page</Route>
+
+    <Route>This is 404 page</Route>
+  </Switch>
   );
 }
 
