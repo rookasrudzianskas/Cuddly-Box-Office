00b83582714fe8874e8be25ce7b6c31e667ececf
Rokas <rookas.rudzenskas@gmail.com>
2021-04-09 07:27:02 +0300

    Rokas

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -5,6 +5,7 @@ import ShowMainData from "../components/show/ShowMainData";
 import Details from "../components/show/Details";
 import Seasons from "../components/show/Seasons";
 import Cast from "../components/show/Cast";
+import {InfoBlock, ShowPageWrapper} from "./Show.styled";
 
 const reducer = (prevState, action) => {
     switch(action.type) {
@@ -59,36 +60,36 @@ const Show = () => {
         return <div>Error occurred: {error}</div>
     }
 
-    return <div>
+    return <ShowPageWrapper>
         <ShowMainData
             image={show.image}
             name={show.name}
             rating={show.rating}
             summary={show.summary}
             tags={show.genres}/>
-        <div>
+        <InfoBlock>
             <h2>Details</h2>
             <Details
             status={show.status}
             network={show.network}
             premiered={show.premiered}
             />
-        </div>
+        </InfoBlock>
 
-        <div>
+        <InfoBlock>
             <h2>Seasons</h2>
             <Seasons
             seasons={show._embedded.seasons}
             />
-        </div>
+        </InfoBlock>
 
-        <div>
+        <InfoBlock>
             <h2>Cast</h2>
             <Cast cast={show._embedded.cast}
             />
-        </div>
+        </InfoBlock>
 
-    </div>;
+    </ShowPageWrapper>;
 };
 
 export default Show;
