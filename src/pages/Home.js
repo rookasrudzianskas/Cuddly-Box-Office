b435b5fb7b2cc14478656406b92281f5fe0dd5ab
Rokas <rookas.rudzenskas@gmail.com>
2021-04-12 07:53:05 +0300

    Rokas

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,9 @@ import {apiGet} from "../misc/config";
 import ShowGrid from "../components/show/ShowGrid";
 import ActorGrid from "../components/actor/ActorGrid";
 import {useLastQuery} from "../misc/custom-hooks";
+import {RadioInputsWrapper, SearchButtonWrapper, SearchInput} from "./Home.styled";
+import CustomRadio from "../components/CustomRadio";
+
 
 const Home = () => {
     const [input, setInput] = useLastQuery();
@@ -53,21 +56,22 @@ const Home = () => {
 
     return (
         <MainPageLayout>
-            <input type="text" placeholder="Search for something" onChange={onInputChange}  onKeyDown={onKeyDown} value={input} />
+            <SearchInput type="text" placeholder="Search for something" onChange={onInputChange}  onKeyDown={onKeyDown} value={input} />
 
-            <div>
-                <label htmlFor="shows-search">
-                    Shows
-                    <input id="shows-search" type="radio" value="shows" onChange={onRadioChange} checked={isShowsSearch}/>
-                </label>
+            <RadioInputsWrapper>
+                <div>
+                    <CustomRadio label="Shows" id="shows-search" value="shows" checked={isShowsSearch} onChange={onRadioChange}/>
+                </div>
+                <div>
                 <label htmlFor="actors-search">
-                    Actors
-                    <input id="actors-search" type="radio" value="people" onChange={onRadioChange} checked={!isShowsSearch}/>
+                    <CustomRadio label="Actors" id="actors-search" value="people" checked={!isShowsSearch} onChange={onRadioChange}/>
                 </label>
-            </div>
+                </div>
+            </RadioInputsWrapper>
 
-
+        <SearchButtonWrapper>
             <button type="button" onClick={onSearch}>Search</button>
+        </SearchButtonWrapper>
             {renderResults()}
         </MainPageLayout>
     );
